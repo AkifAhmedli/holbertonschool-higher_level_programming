29089c1897d3c096faee79a72667ad340c5cc989
AkifAhmedli <akifahmedli007@gmail.com>
2026-02-10 14:48:31 +0000

    Added

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end="")
+            print("FizzBuzz", end=" ")
         elif i % 3 == 0:
-            print("Fizz", end="")
+            print("Fizz", end=" ")
         elif i % 5 == 0:
-            print("Buzz", end="")
+            print("Buzz", end=" ")
         else:
-            print("{}".format(i), end="") 
-        # Hər elementdən sonra, hətta sondan sonra da boşluq qoyuruq
-        print(" ", end="")
+            print(i, end=" ")
