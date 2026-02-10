ddc5975e7c9232f33d191cd57d6513892cd4ac7b
AkifAhmedli <akifahmedli007@gmail.com>
2026-02-10 14:39:01 +0000

    Added

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    """Ədədin sonuncu rəqəmini çap edir və qaytarır."""
-    # Ədədin mütləq qiymətinin 10-a bölünməsindən qalan qalığı tapırıq
+
     last_digit = abs(number) % 10
-    
-    # Sonuncu rəqəmi yeni sətir olmadan çap edirik
+
     print("{}".format(last_digit), end="")
-    
-    # Tapdığımız rəqəmi geri qaytarırıq (Return)
+
     return last_digit
