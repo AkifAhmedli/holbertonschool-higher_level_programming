874a60badd496b5c9745fdcee6737ae1b82d4deb
AkifAhmedli <akifahmedli007@gmail.com>
2026-02-28 12:21:29 +0000

    Added

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-Bu modul obyektin sinif və ya miras mənsubiyyətini yoxlayan funksiyanı ehtiva edir.
+Bu modul obyektin sinif mənsubiyyətini yoxlayan funksiyanı ehtiva edir.
 """
 
 
 def is_kind_of_class(obj, a_class):
-    """Obyektin bir sinifdən və ya ondan miras alan sinifdən olmasını yoxlayır.
+    """Obyektin sinif və ya miras mənsubiyyətini yoxlayır.
 
     Args:
         obj: Yoxlanılacaq obyekt.
         a_class: Müqayisə ediləcək sinif.
 
     Returns:
-        bool: Əgər obyekt a_class-ın instansiyasıdırsa və ya a_class-dan
-        miras alan bir sinfin instansiyasıdırsa True, əks halda False.
+        bool: a_class instansiyası və ya ondan miras alıbsa True.
     """
     return isinstance(obj, a_class)
