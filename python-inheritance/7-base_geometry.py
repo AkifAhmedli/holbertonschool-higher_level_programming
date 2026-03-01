8e7f50c224d00f8cca04bce711b26b9ed2775674
AkifAhmedli <akifahmedli007@gmail.com>
2026-03-01 12:25:12 +0000

    Added

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-"""BaseGeometry modulu."""
+"""Module containing BaseGeometry class"""
 
 
 class BaseGeometry:
-    """BaseGeometry klası."""
+    """Class for BaseGeometry"""
 
     def area(self):
-        """Sahəni hesablayan metod."""
+        """Method to raise exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Tam ədədi yoxlayan metod."""
+        """Method to validate value"""
+
+        # Checking if value is not an int
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
+
+        # Checking if value is not less or equal to 0
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
