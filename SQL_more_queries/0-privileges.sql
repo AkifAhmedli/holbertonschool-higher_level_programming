159fca7e9f72e99c3872cb707280392a12ed830c
AkifAhmedli <akifahmedli007@gmail.com>
2026-03-14 10:09:08 +0000

    asfdf

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- 'user_0d_1' istifadəçisinin bütün icazələrini siyahılayır
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- 'user_0d_2' istifadəçisinin bütün icazələrini siyahılayır
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
