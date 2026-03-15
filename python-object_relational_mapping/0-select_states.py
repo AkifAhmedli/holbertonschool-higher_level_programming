c973a32e92523e279ee7653b3125eb979e7ef231
AkifAhmedli <akifahmedli007@gmail.com>
2026-03-15 14:18:23 +0000

    sgr

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,39 +1,23 @@
 #!/usr/bin/python3
-"""
-hbtn_0e_0_usa verilənlər bazasından bütün ştatları siyahılayan skript.
-Bu skript 3 arqument qəbul edir: mysql istifadəçi adı, şifrə və baza adı.
-"""
-import MySQLdb
-import sys
+"""Module for Selecting states"""
 
-if __name__ == "__main__":
-    # Komanda sətrindən arqumentlərin oxunması
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
 
-    # Verilənlər bazasına qoşulma
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_user,
-        passwd=mysql_password,
-        db=db_name
+        user=argv[1],
+        password=argv[2],
+        database=argv[3]
     )
-
-    # Kursor obyektinin yaradılması (sorğuları icra etmək üçün)
     cursor = db.cursor()
 
-    # SQL sorğusunun icrası
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute('SELECT * FROM states')
 
-    # Bütün nəticələrin götürülməsi
-    rows = cursor.fetchall()
+    for state in cursor.fetchall():
+        print(state)
 
-    # Nəticələrin nümunədəki kimi çap edilməsi
-    for row in rows:
-        print(row)
-
-    # Bağlantıların bağlanması
-    cursor.close()
-    db.close()
+    if cursor:
+        cursor.close()
+    if db:
+        db.close()
