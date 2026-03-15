f5d922be09b196311fa4abd4851e60cf8cac6348
AkifAhmedli <akifahmedli007@gmail.com>
2026-03-15 19:12:03 +0000

    rghr

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
-"""
-Lists all City objects from the database hbtn_0e_14_usa.
-"""
+"""Script that prints all City objects from the database hbtn_0e_14_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
-
 if __name__ == "__main__":
-    # 1. Setup connection
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]),
+            username, password, database
+        ),
         pool_pre_ping=True
     )
 
-    # 2. Create session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # 3. Query both State and City using a join
-    # We sort by City.id as requested
-    results = session.query(State, City)\
-                     .filter(State.id == City.state_id)\
-                     .order_by(City.id)\
-                     .all()
+    results = session.query(State, City).filter(
+        State.id == City.state_id
+    ).order_by(City.id.asc()).all()
 
-    # 4. Print results: <state name>: (<city id>) <city name>
     for state, city in results:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
-    # 5. Close session
     session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""
-Contains the class definition of a City.
-"""
+"""Module that defines the City class linked to the cities table"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 
 class City(Base):
-    """
-    City class:
-    - inherits from Base (imported from model_state)
-    - links to the MySQL table cities
-    """
+    """City class that links to the MySQL table cities"""
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
